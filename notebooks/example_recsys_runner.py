e0997d74f549e8d341f09c12728eb9c21269ec7f
Jeongmin Lee <jlee@cs.pitt.edu>
2020-06-23 07:07:12 -0400

    hugginface-based RecSys transformer runner file update

--- notebooks/example_recsys_runner.py
+++ notebooks/example_recsys_runner.py
@@ -126,7 +126,6 @@ def main():
     train_data_path = [
         d_path + "session_start_date=2019-10-01",
         d_path + "session_start_date=2019-10-02",
-        # d_path + "session_start_date=2019-10-03",
     ]
 
     # data B
@@ -201,22 +200,19 @@ def main():
     train_loader = DataLoaderWithLen(
         make_batch_reader(train_data_path, 
             num_epochs=None,
-            # transform_spec=transform
             schema_fields=recsys_schema_small,
         ), 
         batch_size=training_args.per_device_train_batch_size,
-        # collate_fn=data_collator,
         len = TRAIN_DATA_LEN,
     )
 
     eval_loader = DataLoaderWithLen(
         make_batch_reader(eval_data_path, 
             num_epochs=None,
-            # transform_spec=transform
             schema_fields=recsys_schema_full,
         ), 
         batch_size=training_args.per_device_eval_batch_size,
-        # collate_fn=data_collator,
+
         len=EVAL_DATA_LEN,
     )
 
@@ -236,16 +232,16 @@ def main():
         dropout=0.1,
     )
 
-    # if model_args.model_name_or_path:
-    #     model = XLNetLMHeadModel.from_pretrained(
-    #         model_args.model_name_or_path,
-    #         from_tf=bool(".ckpt" in model_args.model_name_or_path),
-    #         config=config,
-    #         cache_dir=model_args.cache_dir,
-    #     )
-    # else:
-    logger.info("Training new model from scratch")
-    model = XLNetLMHeadModel(config)
+    if model_args.model_name_or_path:
+        model = XLNetLMHeadModel.from_pretrained(
+            model_args.model_name_or_path,
+            from_tf=bool(".ckpt" in model_args.model_name_or_path),
+            config=config,
+            cache_dir=model_args.cache_dir,
+        )
+    else:
+        logger.info("Training new model from scratch")
+        model = XLNetLMHeadModel(config)
 
     trainer = RecSysTrainer(
         train_loader=train_loader, 
@@ -288,151 +284,6 @@ def main():
         results.update(result)
 
     return results
-
-# def main():
-#     # See all possible arguments in src/transformers/training_args.py
-#     # or by passing the --help flag to this script.
-#     # We now keep distinct sets of args, for a cleaner separation of concerns.
-
-#     parser = HfArgumentParser((TrainingArguments))
-#     training_args = parser.parse_args_into_dataclasses()
-
-#     if (
-#         os.path.exists(training_args.output_dir)
-#         and os.listdir(training_args.output_dir)
-#         and training_args.do_train
-#         and not training_args.overwrite_output_dir
-#     ):
-#         raise ValueError(
-#             f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
-#         )
-
-#     # Setup logging
-#     logging.basicConfig(
-#         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
-#         datefmt="%m/%d/%Y %H:%M:%S",
-#         level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
-#     )
-#     logger.warning(
-#         "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
-#         training_args.local_rank,
-#         training_args.device,
-#         training_args.n_gpu,
-#         bool(training_args.local_rank != -1),
-#         training_args.fp16,
-#     )
-#     logger.info("Training/evaluation parameters %s", training_args)
-
-#     # Set seed
-#     set_seed(training_args.seed)
-
-#     # Load pretrained model and tokenizer
-#     #
-#     # Distributed training:
-#     # The .from_pretrained methods guarantee that only one local process can concurrently
-#     # download model & vocab.
-
-#     if model_args.config_name:
-#         config = AutoConfig.from_pretrained(model_args.config_name, cache_dir=model_args.cache_dir)
-#     elif model_args.model_name_or_path:
-#         config = AutoConfig.from_pretrained(model_args.model_name_or_path, cache_dir=model_args.cache_dir)
-#     else:
-#         config = CONFIG_MAPPING[model_args.model_type]()
-#         logger.warning("You are instantiating a new config instance from scratch.")
-
-#     if model_args.tokenizer_name:
-#         tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, cache_dir=model_args.cache_dir)
-#     elif model_args.model_name_or_path:
-#         tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, cache_dir=model_args.cache_dir)
-#     else:
-#         raise ValueError(
-#             "You are instantiating a new tokenizer from scratch. This is not supported, but you can do it from another script, save it,"
-#             "and load it from here, using --tokenizer_name"
-#         )
-
-#     if model_args.model_name_or_path:
-#         model = AutoModelWithLMHead.from_pretrained(
-#             model_args.model_name_or_path,
-#             from_tf=bool(".ckpt" in model_args.model_name_or_path),
-#             config=config,
-#             cache_dir=model_args.cache_dir,
-#         )
-#     else:
-#         logger.info("Training new model from scratch")
-#         model = AutoModelWithLMHead.from_config(config)
-
-#     model.resize_token_embeddings(len(tokenizer))
-
-#     if config.model_type in ["bert", "roberta", "distilbert", "camembert"] and not data_args.mlm:
-#         raise ValueError(
-#             "BERT and RoBERTa-like models do not have LM heads but masked LM heads. They must be run using the --mlm "
-#             "flag (masked language modeling)."
-#         )
-
-#     if data_args.block_size <= 0:
-#         data_args.block_size = tokenizer.max_len
-#         # Our input block size will be the max possible for the model
-#     else:
-#         data_args.block_size = min(data_args.block_size, tokenizer.max_len)
-
-#     # Get datasets
-
-#     train_dataset = get_dataset(data_args, tokenizer=tokenizer) if training_args.do_train else None
-#     eval_dataset = get_dataset(data_args, tokenizer=tokenizer, evaluate=True) if training_args.do_eval else None
-#     data_collator = DataCollatorForLanguageModeling(
-#         tokenizer=tokenizer, mlm=data_args.mlm, mlm_probability=data_args.mlm_probability
-#     )
-
-#     # Initialize our Trainer
-#     trainer = Trainer(
-#         model=model,
-#         args=training_args,
-#         data_collator=data_collator,
-#         train_dataset=train_dataset,
-#         eval_dataset=eval_dataset,
-#         prediction_loss_only=True,
-#     )
-
-#     # Training
-#     if training_args.do_train:
-#         model_path = (
-#             model_args.model_name_or_path
-#             if model_args.model_name_or_path is not None and os.path.isdir(model_args.model_name_or_path)
-#             else None
-#         )
-#         trainer.train(model_path=model_path)
-#         trainer.save_model()
-#         # For convenience, we also re-save the tokenizer to the same directory,
-#         # so that you can share your model easily on huggingface.co/models =)
-#         if trainer.is_world_master():
-#             tokenizer.save_pretrained(training_args.output_dir)
-
-#     # Evaluation
-#     results = {}
-#     if training_args.do_eval:
-#         logger.info("*** Evaluate ***")
-
-#         eval_output = trainer.evaluate()
-
-#         perplexity = math.exp(eval_output["eval_loss"])
-#         result = {"perplexity": perplexity}
-
-#         output_eval_file = os.path.join(training_args.output_dir, "eval_results_lm.txt")
-#         if trainer.is_world_master():
-#             with open(output_eval_file, "w") as writer:
-#                 logger.info("***** Eval results *****")
-#                 for key in sorted(result.keys()):
-#                     logger.info("  %s = %s", key, str(result[key]))
-#                     writer.write("%s = %s\n" % (key, str(result[key])))
-
-#         results.update(result)
-
-#     return results
-
-
-# def _mp_fn(index):
-#     # For xla_spawn (TPUs)
-#     main()
 
 
 if __name__ == "__main__":
