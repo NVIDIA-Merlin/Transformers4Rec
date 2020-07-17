fc5ea409071f2da3433c2149e8499f7528ca169c
Jeongmin Lee <jlee@cs.pitt.edu>
2020-07-17 03:14:33 -0400

    add/update scripts to setup experiment environment and get dataset

---
+++ codes/script/setup_env.bash
@@ -0,0 +1,23 @@
+# environment setup right after getting a ngc instance and clone recsys repo
+
+apt-get install sshfs -y
+apt-get install unzip -y
+apt-get install screen -y
+apt-get install nano -y
+
+# create new virtual env and activate
+conda create -n recsys python=3.7
+source activate recsys
+
+cd ~/recsys/transformers4recsys/codes
+
+# install python packages
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch --yes
+pip install gpustat --yes
+pip install -r requirements.txt
+
+# get dataset
+bash bash script/get_dataset.bash
+
+# run experiment
+bash script/run_transformer.bash
