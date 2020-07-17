f0afcd24d6fce6302b25512e9cde5c2367cce2cf
Jeongmin Lee <jlee@cs.pitt.edu>
2020-07-17 03:32:29 -0400

    update get_dataset.bash

--- codes/script/get_dataset.bash
+++ codes/script/get_dataset.bash
@@ -1,15 +1,25 @@
 pip install gdown
 
-mkdir -p ~/dataset
-cd ~/dataset
+unzip_d () {
+    zipfile="$1"
+    zipdir=${1%.zip}
+    unzip -d "$zipdir" "$zipfile"
+}
+
+cur_path=$(pwd)
+
+target_path="~/dataset"
+mkdir -p ${target_path}
+
+cd ${target_path}
 
 gdown https://drive.google.com/uc?id=1f0PV3CLyN1EYzmLXBDeprqST-rf1ZaY3
 unzip ecommerce_preproc_neg_samples_50_strategy_cooccurrence_19_days_first_10k_sessions.parquet.zip
 
 gdown https://drive.google.com/uc?id=1AfyZ75KH7XwOWHZ7qY0CDix9yxIn1ejL
-unzip ecommerce_preproc_neg_samples_50_strategy_uniform-2019-10.zip
+unzip_d ecommerce_preproc_neg_samples_50_strategy_uniform-2019-10.zip
 
 gdown https://drive.google.com/uc?id=1Z5e4qjRNSM36zaOAJ1zWV9WXZZVGR2sh
-unzip ecommerce_preproc_neg_samples_50_strategy_recent_popularity-2019-10.zip
+unzip_d ecommerce_preproc_neg_samples_50_strategy_recent_popularity-2019-10.zip
 
-cd -
+cd ${cur_path}
