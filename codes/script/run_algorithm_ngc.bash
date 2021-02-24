e903a6c9df26f019b5942dce3c2a4e83c8ede76d
Gabriel Moreira <gmoreira@nvidia.com>
2021-02-24 13:08:01 -0300

    Added generic bash script to run both the Transformers and baselines on NGC

---
+++ codes/script/run_algorithm_ngc.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir -p ./tmp/
+
+python_script=$1
+experiments_group=$2 # Name of the experiment group for this run. Used only to organize jobs in W&B
+
+echo "Running training script"
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0,1 python ${python_script} \
+    --output_dir "./tmp/" \
+    --overwrite_output_dir \
+    --do_train \
+    --do_eval \
+    --data_path "/data/" \
+    --data_loader_engine nvtabular \
+    --validate_every 10 \
+    --logging_steps 20 \
+    --save_steps 0 \
+    --experiments_group ${experiments_group} \
+    ${@:3} # Forwarding Remaining parameters to the script
+
+train_exit_status=$?    
+    
+echo "Copying outputs to NGC results dir"
+cp -r ./tmp/pred_logs/ /results
+cp -r ./tmp/attention_weights/ /results
+cp ./tmp/*.txt /results
+cp ./tmp/*.json /results
+cp ./tmp/*.csv /results
+cp -r ./runs/ /results
+
+#Ensuring that the job exit status is the same of the trainings scripts
+exit ${train_exit_status}
