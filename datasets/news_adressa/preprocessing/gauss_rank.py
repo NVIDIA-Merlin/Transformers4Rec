aca4626208b9e93a5ebdc7be388caec9248da215
Gabriel Moreira <gmoreira@nvidia.com>
2021-04-12 18:45:03 -0300

    Added preproc of the Adressa news dataset (first 16 days) based on the preproc from the CHAMELEON framework

---
+++ datasets/news_adressa/preprocessing/gauss_rank.py
@@ -0,0 +1,118 @@
+import numpy as np
+from joblib import Parallel, delayed
+from scipy.interpolate import interp1d
+from scipy.special import erf, erfinv
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import FLOAT_DTYPES, check_array, check_is_fitted
+
+
+class GaussRankScaler(BaseEstimator, TransformerMixin):
+    """Transform features by scaling each feature to a normal distribution.
+    Parameters
+        ----------
+        epsilon : float, optional, default 1e-4
+            A small amount added to the lower bound or subtracted
+            from the upper bound. This value prevents infinite number
+            from occurring when applying the inverse error function.
+        copy : boolean, optional, default True
+            If False, try to avoid a copy and do inplace scaling instead.
+            This is not guaranteed to always work inplace; e.g. if the data is
+            not a NumPy array, a copy may still be returned.
+        n_jobs : int or None, optional, default None
+            Number of jobs to run in parallel.
+            ``None`` means 1 and ``-1`` means using all processors.
+        interp_kind : str or int, optional, default 'linear'
+           Specifies the kind of interpolation as a string
+            ('linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic',
+            'previous', 'next', where 'zero', 'slinear', 'quadratic' and 'cubic'
+            refer to a spline interpolation of zeroth, first, second or third
+            order; 'previous' and 'next' simply return the previous or next value
+            of the point) or as an integer specifying the order of the spline
+            interpolator to use.
+        interp_copy : bool, optional, default False
+            If True, the interpolation function makes internal copies of x and y.
+            If False, references to `x` and `y` are used.
+        Attributes
+        ----------
+        interp_func_ : list
+            The interpolation function for each feature in the training set.
+        """
+
+    def __init__(self, epsilon=1e-4, copy=True, n_jobs=None, interp_kind='linear', interp_copy=False):
+        self.epsilon = epsilon
+        self.copy = copy
+        self.interp_kind = interp_kind
+        self.interp_copy = interp_copy
+        self.fill_value = 'extrapolate'
+        self.n_jobs = n_jobs
+
+    def fit(self, X, y=None):
+        """Fit interpolation function to link rank with original data for future scaling
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data used to fit interpolation function for later scaling along the features axis.
+        y
+            Ignored
+        """
+        X = check_array(X, copy=self.copy, estimator=self, dtype=FLOAT_DTYPES, force_all_finite=True)
+
+        self.interp_func_ = Parallel(n_jobs=self.n_jobs)(delayed(self._fit)(x) for x in X.T)
+        return self
+
+    def _fit(self, x):
+        x = self.drop_duplicates(x)
+        rank = np.argsort(np.argsort(x))
+        bound = 1.0 - self.epsilon
+        factor = np.max(rank) / 2.0 * bound
+        scaled_rank = np.clip(rank / factor - bound, -bound, bound)
+        return interp1d(
+            x, scaled_rank, kind=self.interp_kind, copy=self.interp_copy, fill_value=self.fill_value)
+
+    def transform(self, X, copy=None):
+        """Scale the data with the Gauss Rank algorithm
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data used to scale along the features axis.
+        copy : bool, optional (default: None)
+            Copy the input X or not.
+        """
+        check_is_fitted(self, 'interp_func_')
+
+        copy = copy if copy is not None else self.copy
+        X = check_array(X, copy=copy, estimator=self, dtype=FLOAT_DTYPES, force_all_finite=True)
+
+        X = np.array(Parallel(n_jobs=self.n_jobs)(delayed(self._transform)(i, x) for i, x in enumerate(X.T))).T
+        return X
+
+    def _transform(self, i, x):
+        return erfinv(self.interp_func_[i](x))
+
+    def inverse_transform(self, X, copy=None):
+        """Scale back the data to the original representation
+        Parameters
+        ----------
+        X : array-like, shape [n_samples, n_features]
+            The data used to scale along the features axis.
+        copy : bool, optional (default: None)
+            Copy the input X or not.
+        """
+        check_is_fitted(self, 'interp_func_')
+
+        copy = copy if copy is not None else self.copy
+        X = check_array(X, copy=copy, estimator=self, dtype=FLOAT_DTYPES, force_all_finite=True)
+
+        X = np.array(Parallel(n_jobs=self.n_jobs)(delayed(self._inverse_transform)(i, x) for i, x in enumerate(X.T))).T
+        return X
+
+    def _inverse_transform(self, i, x):
+        inv_interp_func = interp1d(self.interp_func_[i].y, self.interp_func_[i].x, kind=self.interp_kind,
+                                   copy=self.interp_copy, fill_value=self.fill_value)
+        return inv_interp_func(erf(x))
+
+    @staticmethod
+    def drop_duplicates(x):
+        is_unique = np.zeros_like(x, dtype=bool)
+        is_unique[np.unique(x, return_index=True)[1]] = True
+        return x[is_unique]
