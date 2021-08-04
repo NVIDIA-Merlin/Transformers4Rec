2b3c49bd6238250e9c0d37a14e28904eba2314e8
rnyak <ronayak@hotmail.com>
2021-08-04 17:18:54 -0400

    Update recsys_data.py

--- transformers4rec/recsys_data.py
+++ transformers4rec/recsys_data.py
@@ -208,7 +208,9 @@ def get_nvtabular_dataloader(
 
     # device_key = "devices" if nvtabular.__version__ < "0.5.1" else "device"
     dataloader_device = 0 if training_args.local_rank == -1 else training_args.local_rank
-   # Use either part_mem_fraction or part_size (default None) arguments. Each can be set with model training script. 
+   # Use either part_mem_fraction or part_size (default None) arguments. Cannot use both at the same time.
+   # If doing multi-gpu distributed data parallel training, set one of the arguments accordingly, such that dataset row_group 
+   # memory size should be smaller than the partition size and number of row groups per file should be >= number of gpus in use.
    # If using part_mem_fraction param, replace "part_size" line with "part_mem_fraction=data_args.nvt_part_mem_fraction". 
     dataset = NVTDataset(
         data_paths,
