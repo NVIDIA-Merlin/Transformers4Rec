b0a4932489ee515378c1d947df09e0f59e2dde38
sararb <sara.rabhi@gmail.com>
2021-08-12 18:40:04 +0000

    update test with schema

--- tests/torch/block/test_transformer.py
+++ tests/torch/block/test_transformer.py
@@ -11,9 +11,9 @@ lm_tasks = list(torch4rec.masking.masking_registry.keys())
 
 # Test output of XLNet with different masking taks using SequentialBlock
 @pytest.mark.parametrize("task", lm_tasks)
-def test_transformer_block(yoochoose_column_group, torch_yoochoose_like, task):
-    col_group = yoochoose_column_group
-    tab_module = torch4rec.SequentialTabularFeatures.from_column_group(
+def test_transformer_block(yoochoose_schema, torch_yoochoose_like, task):
+    col_group = yoochoose_schema
+    tab_module = torch4rec.SequentialTabularFeatures.from_schema(
         col_group, max_sequence_length=20, aggregation="sequential_concat"
     )
 
@@ -33,9 +33,9 @@ def test_transformer_block(yoochoose_column_group, torch_yoochoose_like, task):
 
 # Test output of XLNet with different masking taks using pytorch-like code
 @pytest.mark.parametrize("task", lm_tasks)
-def test_transformer_block_torch_like(yoochoose_column_group, torch_yoochoose_like, task):
-    col_group = yoochoose_column_group
-    tab_module = torch4rec.SequentialTabularFeatures.from_column_group(
+def test_transformer_block_torch_like(yoochoose_schema, torch_yoochoose_like, task):
+    col_group = yoochoose_schema
+    tab_module = torch4rec.SequentialTabularFeatures.from_schema(
         col_group, max_sequence_length=20, aggregation="sequential_concat"
     )
 
