d4648a577e88c80e2277140d5d44f3dd92c4d1bd
sararb <sara.rabhi@gmail.com>
2021-08-16 03:05:50 -0400

    fix moving the tabular module to appropriate device (#113)
    * fix moving the tabular module to appropriate device
    
    * Check if gpu is available
    
    Set the list of devices to test based on the gpu availability.
    
    * fix formatting
    
    * remove whitespace
    
    Co-authored-by: Ben Frederickson <github@benfrederickson.com>

--- tests/torch/test_tabular.py
+++ tests/torch/test_tabular.py
@@ -1,6 +1,12 @@
 import pytest
+import torch
 
 torch4rec = pytest.importorskip("transformers4rec.torch")
+
+if torch.cuda.is_available():
+    devices = ["cpu", "cuda"]
+else:
+    devices = ["cpu"]
 
 
 def test_filter_features(torch_con_features):
@@ -37,3 +43,31 @@ def test_tabular_module(torch_con_features):
 
     assert tab_a(torch_con_features, merge_with=tab_b, stack_outputs=True).size()[1] == 1
     assert (tab_a + tab_b)(torch_con_features, concat_outputs=True).size()[1] == 2
+
+
+@pytest.mark.parametrize("device", devices)
+def test_tabular_module_to_device(yoochoose_schema, device):
+    schema = yoochoose_schema
+    tab_module = torch4rec.SequentialTabularFeatures.from_schema(
+        schema, max_sequence_length=20, aggregation="sequential_concat"
+    )
+    tab_module.to(device)
+
+    # Flatten nested torch modules
+    def flatten(el):
+        flattened = [flatten(children) for children in el.children()]
+        res = [el]
+        for c in flattened:
+            res += c
+        return res
+
+    flatten_layers = flatten(tab_module)
+
+    # Check params of pytorch modules are moved to appropriate device
+    assert all(
+        [
+            list(el.parameters())[-1].device.type == device
+            for el in flatten_layers
+            if list(el.parameters())
+        ]
+    )
