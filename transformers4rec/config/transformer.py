18e68731238760187f3ddf44d69ab8ab5b9145e0
Marc Romeyn <marcromeyn@gmail.com>
2021-08-18 11:20:20 +0200

    Roll back to main

--- transformers4rec/config/transformer.py
+++ transformers4rec/config/transformer.py
@@ -21,14 +21,14 @@ class T4RecConfig:
         return self.__class__.__bases__[1]
 
     @classmethod
-    def build(cls, *args, **kwargs):
+    def for_rec(cls, *args, **kwargs):
         raise NotImplementedError
 
 
 @transformer_registry.register("reformer")
 class ReformerConfig(T4RecConfig, transformers.ReformerConfig):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
@@ -40,15 +40,13 @@ class ReformerConfig(T4RecConfig, transformers.ReformerConfig):
         dropout=0.3,
         pad_token=0,
         log_attention_weights=True,
-        axial_pos_shape_first_dim=4,
         **kwargs
     ):
         return cls(
-            hidden_size=d_model,
             attention_head_size=d_model,
             attn_layers=["local", "lsh"] * (n_layer // 2) if n_layer > 2 else ["local"],
-            num_hidden_layers=n_layer,
             feed_forward_size=d_model * 4,
+            hidden_size=d_model,
             num_attention_heads=n_head,
             hidden_act=hidden_act,
             initializer_range=initializer_range,
@@ -62,10 +60,6 @@ class ReformerConfig(T4RecConfig, transformers.ReformerConfig):
                 d_model // 2,
                 d_model // 2,
             ],
-            axial_pos_shape=[
-                axial_pos_shape_first_dim,
-                total_seq_length // axial_pos_shape_first_dim,
-            ],
             vocab_size=1,
             **kwargs
         )
@@ -74,7 +68,7 @@ class ReformerConfig(T4RecConfig, transformers.ReformerConfig):
 @transformer_registry.register("gtp2")
 class GPT2Config(T4RecConfig, transformers.GPT2Config):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
@@ -110,7 +104,7 @@ class GPT2Config(T4RecConfig, transformers.GPT2Config):
 @transformer_registry.register("longformer")
 class LongformerConfig(T4RecConfig, transformers.LongformerConfig):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
@@ -129,10 +123,10 @@ class LongformerConfig(T4RecConfig, transformers.LongformerConfig):
             num_hidden_layers=n_layer,
             num_attention_heads=n_head,
             hidden_act=hidden_act,
-            attention_window=total_seq_length,
             initializer_range=initializer_range,
             layer_norm_eps=layer_norm_eps,
             dropout=dropout,
+            max_position_embeddings=total_seq_length,
             pad_token_id=pad_token,
             output_attentions=log_attention_weights,
             vocab_size=1,
@@ -143,7 +137,7 @@ class LongformerConfig(T4RecConfig, transformers.LongformerConfig):
 @transformer_registry.register("electra")
 class ElectraConfig(T4RecConfig, transformers.ElectraConfig):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
@@ -178,7 +172,7 @@ class ElectraConfig(T4RecConfig, transformers.ElectraConfig):
 @transformer_registry.register("albert")
 class AlbertConfig(T4RecConfig, transformers.AlbertConfig):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
@@ -213,12 +207,11 @@ class AlbertConfig(T4RecConfig, transformers.AlbertConfig):
 @transformer_registry.register("xlnet")
 class XLNetConfig(T4RecConfig, transformers.XLNetConfig):
     @classmethod
-    def build(
+    def for_rec(
         cls,
         d_model,
         n_head,
         n_layer,
-        total_seq_length,
         hidden_act="gelu",
         initializer_range=0.01,
         layer_norm_eps=0.03,
