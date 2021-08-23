40322306d47793c6754cc5b29af5bfd0fb7aab4b
Marc Romeyn <marcromeyn@gmail.com>
2021-08-23 13:12:41 +0200

    Implement tf_yoochoose_like based on pytorch version

--- tests/tf/features/test_continuous.py
+++ tests/tf/features/test_continuous.py
@@ -1,5 +1,7 @@
 import pytest
 
+from transformers4rec.utils.tags import Tag
+
 tf4rec = pytest.importorskip("transformers4rec.tf")
 
 
@@ -8,3 +10,13 @@ def test_continuous_features(tf_con_features):
     con = tf4rec.ContinuousFeatures(features)(tf_con_features)
 
     assert list(con.keys()) == features
+
+
+def test_continuous_features_yoochoose(yoochoose_schema, tf_yoochoose_like):
+    schema = yoochoose_schema
+    cont_cols = schema.select_by_tag(Tag.CONTINUOUS)
+
+    con = tf4rec.ContinuousFeatures.from_schema(cont_cols)
+    outputs = con(tf_yoochoose_like)
+
+    assert list(outputs.keys()) == cont_cols.column_names
