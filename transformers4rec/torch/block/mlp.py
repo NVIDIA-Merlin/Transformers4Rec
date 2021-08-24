19d8aa86ada1e6e4a57dac283b32f61f5387019d
Marc Romeyn <marcromeyn@gmail.com>
2021-08-24 10:17:49 +0200

    Break out _create_layer in MLPBlock to DenseBlock (#137)
    * Break out _create_layer in MLPBlock to DenseBlock
    
    * Throw ValueError if normalization is not a Layer of batch-norm

--- transformers4rec/torch/block/mlp.py
+++ transformers4rec/torch/block/mlp.py
@@ -1,5 +1,3 @@
-import types
-
 import torch
 
 from .base import BuildableBlock, SequentialBlock
@@ -31,7 +29,15 @@ class MLPBlock(BuildableBlock):
         layer_input_sizes = list(input_shape[-1:]) + list(self.dimensions[:-1])
         layer_output_sizes = self.dimensions
         sequential = [
-            self._create_layer(input_size, output_size, input_shape)
+            DenseBlock(
+                input_shape,
+                input_size,
+                output_size,
+                activation=self.activation,
+                use_bias=self.use_bias,
+                dropout=self.dropout,
+                normalization=self.normalization,
+            )
             for input_size, output_size in zip(layer_input_sizes, layer_output_sizes)
         ]
 
@@ -40,35 +46,42 @@ class MLPBlock(BuildableBlock):
 
         return output
 
-    def _create_layer(self, dense_input_size, dense_output_size, input_shape):
-        out = [torch.nn.Linear(dense_input_size, dense_output_size, bias=self.use_bias)]
-        if self.activation:
-            out.append(self.activation(inplace=True))
-        if self.normalization:
-            if self.normalization == "batch_norm":
-                out.append(torch.nn.BatchNorm1d(dense_output_size))
-        if self.dropout:
-            out.append(torch.nn.Dropout(self.dropout))
 
-        output = torch.nn.Sequential(*out)
+class DenseBlock(SequentialBlock):
+    def __init__(
+        self,
+        input_shape,
+        in_features: int,
+        out_features: int,
+        activation=torch.nn.ReLU,
+        use_bias: bool = True,
+        dropout=None,
+        normalization=None,
+    ):
+        args = [torch.nn.Linear(in_features, out_features, bias=use_bias)]
+        if activation:
+            args.append(activation(inplace=True))
+        if normalization:
+            if normalization == "batch_norm":
+                args.append(torch.nn.BatchNorm1d(out_features))
+        if dropout:
+            args.append(torch.nn.Dropout(self.dropout))
 
-        def _get_name(self):
-            return "DenseBlock"
+        super().__init__(*args)
+        self._input_shape = input_shape
+        self._output_size = out_features
 
-        def output_size(self):
-            if len(input_shape) == 3:
-                return torch.Size([input_shape[0], input_shape[1], dense_output_size])
+    def _get_name(self):
+        return "DenseBlock"
 
-            return torch.Size([input_shape[0], dense_output_size])
+    def output_size(self):
+        if len(self._input_shape) == 3:
+            return torch.Size([self._input_shape[0], self._input_shape[1], self._output_size])
 
-        def forward_output_size(self, input_size):
-            if len(input_size) == 3:
-                return torch.Size([input_size[0], input_size[1], dense_output_size])
+        return torch.Size([self._input_shape[0], self._output_size])
 
-            return torch.Size([input_size[0], dense_output_size])
+    def forward_output_size(self, input_size):
+        if len(input_size) == 3:
+            return torch.Size([input_size[0], input_size[1], self._output_size])
 
-        output._get_name = types.MethodType(_get_name, output)
-        output.output_size = types.MethodType(output_size, output)
-        output.forward_output_size = types.MethodType(forward_output_size, output)
-
-        return output
+        return torch.Size([input_size[0], self._output_size])
