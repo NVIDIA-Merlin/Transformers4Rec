927e937ef7b443ffa900be8bc02f1502f42fc55b
Ben Frederickson <github@benfrederickson.com>
2021-09-03 01:13:23 -0700

    Skeleton Sphinx Documentation (#172)
    * Skeleton Sphinx Documentation
    
    * black
    
    * Reformat docstrings and regenerate api docs for latest changes
    
    * minor fixes

---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. Transformers4Rec documentation master file, created by
+   sphinx-quickstart on Tue Aug 31 16:07:09 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Transformers4Rec's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Getting Started <getting_started>
+   Examples <examples/index>
+   API Documentation <api/index>
+   Additional Resources <resources/index>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- transformers4rec/utils/registry.py
+++ transformers4rec/utils/registry.py
@@ -16,12 +16,18 @@ def snakecase_to_camelcase(name):
 
 def default_name(class_or_fn):
     """Default name for a class or function.
+
     This is the naming function by default for registries expecting classes or
     functions.
-    Args:
-      class_or_fn: class or function to be named.
-    Returns:
-      Default name for registration.
+
+    Parameters
+    ----------
+    class_or_fn:
+        class or function to be named.
+
+    Returns
+    -------
+    Default name for registration.
     """
     return camelcase_to_snakecase(class_or_fn.__name__)
 
@@ -31,33 +37,51 @@ def default_object_name(obj):
 
 
 class Registry:
-    """Dict-like class for managing function registrations.
-    ```python
-    my_registry = Registry("custom_name")
-    @my_registry.register
-    def my_func():
-      pass
-    @my_registry.register()
-    def another_func():
-      pass
-    @my_registry.register("non_default_name")
-    def third_func(x, y, z):
-      pass
-    def foo():
-      pass
-    my_registry.register()(foo)
-    my_registry.register("baz")(lambda (x, y): x + y)
-    my_register.register("bar")
-    print(list(my_registry))
-    # ["my_func", "another_func", "non_default_name", "foo", "baz"]
-    # (order may vary)
-    print(my_registry["non_default_name"] is third_func)  # True
-    print("third_func" in my_registry)                    # False
-    print("bar" in my_registry)                           # False
-    my_registry["non-existent_key"]                       # raises KeyError
-    ```
+    """
+    Dict-like class for managing function registrations.
+
+    Example usage::
+
+        my_registry = Registry("custom_name")
+        @my_registry.register
+        def my_func():
+          pass
+        @my_registry.register()
+        def another_func():
+          pass
+        @my_registry.register("non_default_name")
+        def third_func(x, y, z):
+          pass
+        def foo():
+          pass
+        my_registry.register()(foo)
+        my_registry.register("baz")(lambda (x, y): x + y)
+        my_register.register("bar")
+        print(list(my_registry))
+        # ["my_func", "another_func", "non_default_name", "foo", "baz"]
+        # (order may vary)
+        print(my_registry["non_default_name"] is third_func)  # True
+        print("third_func" in my_registry)                    # False
+        print("bar" in my_registry)                           # False
+        my_registry["non-existent_key"]                       # raises KeyError
+
     Optional validation, on_set callback and value transform also supported.
-    See `__init__` doc.
+
+    Parameters
+    ----------
+    registry_name: str
+        identifier for the given registry. Used in error msgs.
+    default_key_fn: callable, optional
+        function mapping value -> key for registration when a key is not provided
+    validator: callable, optional
+        if given, this is run before setting a given (key, value) pair. Accepts (key, value)
+        and should raise if there is a problem. Overwriting existing keys is not allowed and is
+        checked separately. Values are also checked to be callable separately.
+    on_set: callable, optional
+        callback function accepting (key, value) pair which is run after an item is successfully
+        set.
+    value_transformer: callable, optional
+        if run, `__getitem__` will return value_transformer(key, registered_value).
     """
 
     def __init__(
@@ -68,20 +92,6 @@ class Registry:
         on_set=None,
         value_transformer=(lambda k, v: v),
     ):
-        """Construct a new registry.
-        Args:
-          registry_name: str identifier for the given registry. Used in error msgs.
-          default_key_fn (optional): function mapping value -> key for registration
-            when a key is not provided
-          validator (optional): if given, this is run before setting a given (key,
-            value) pair. Accepts (key, value) and should raise if there is a
-            problem. Overwriting existing keys is not allowed and is checked
-            separately. Values are also checked to be callable separately.
-          on_set (optional): callback function accepting (key, value) pair which is
-            run after an item is successfully set.
-          value_transformer (optional): if run, `__getitem__` will return
-            value_transformer(key, registered_value).
-        """
         self._registry = {}
         self._name = registry_name
         self._default_key_fn = default_key_fn
@@ -121,12 +131,16 @@ class Registry:
 
     def __setitem__(self, key, value):
         """Validate, set, and (if successful) call `on_set` for the given item.
-        Args:
-          key: key to store value under. If `None`, `self.default_key(value)` is
-            used.
-          value: callable stored under the given key.
-        Raises:
-          KeyError: if key is already in registry.
+
+        Parameters
+        ----------
+        key:
+            key to store value under. If `None`, `self.default_key(value)` is used.
+        value:
+            callable stored under the given key.
+        Raises
+        ------
+            KeyError: if key is already in registry.
         """
         if key is None:
             key = self.default_key(value)
@@ -142,34 +156,39 @@ class Registry:
         """Decorator to register a function, or registration itself.
         This is primarily intended for use as a decorator, either with or without
         a key/parentheses.
-        ```python
-        @my_registry.register('key1')
-        def value_fn(x, y, z):
-          pass
-        @my_registry.register()
-        def another_fn(x, y):
-          pass
-        @my_registry.register
-        def third_func():
-          pass
-        ```
+
+        Example Usage::
+
+            @my_registry.register('key1')
+            def value_fn(x, y, z):
+              pass
+            @my_registry.register()
+            def another_fn(x, y):
+              pass
+            @my_registry.register
+            def third_func():
+              pass
+
         Note if key_or_value is provided as a non-callable, registration only
         occurs once the returned callback is called with a callable as its only
-        argument.
-        ```python
-        callback = my_registry.register('different_key')
-        'different_key' in my_registry  # False
-        callback(lambda (x, y): x + y)
-        'different_key' in my_registry  # True
-        ```
-        Args:
-          key_or_value (optional): key to access the registered value with, or the
-            function itself. If `None` (default), `self.default_key` will be called
-            on `value` once the returned callback is called with `value` as the only
-            arg. If `key_or_value` is itself callable, it is assumed to be the value
-            and the key is given by `self.default_key(key)`.
-        Returns:
-          decorated callback, or callback generated a decorated function.
+        argument::
+
+            callback = my_registry.register('different_key')
+            'different_key' in my_registry  # False
+            callback(lambda (x, y): x + y)
+            'different_key' in my_registry  # True
+
+        Parameters
+        ----------
+        key_or_value (optional):
+            key to access the registered value with, or the unction itself. If `None` (default),
+            `self.default_key` will be called on `value` once the returned callback is called with
+            `value` as the only arg. If `key_or_value` is itself callable, it is assumed to be the
+            value and the key is given by `self.default_key(key)`.
+
+        Returns
+        -------
+        decorated callback, or callback generated a decorated function.
         """
 
         def decorator(value, key):
