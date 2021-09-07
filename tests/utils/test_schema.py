e41cfcf4c3253262244f96c1801b5db91c43b124
Marc Romeyn <marcromeyn@gmail.com>
2021-09-07 10:54:24 +0200

    Trying to fix failing tests

--- tests/utils/test_schema.py
+++ tests/utils/test_schema.py
@@ -5,14 +5,14 @@ from transformers4rec.utils.tags import Tag
 
 
 def test_schema_from_schema(schema_file):
-    schema = DatasetSchema.from_schema(str(schema_file))
+    schema = DatasetSchema.from_proto(str(schema_file))
 
     assert len(schema.columns) == 18
     assert schema.columns[1].tags == ["list"]
 
 
 def test_schema_from_yoochoose_schema(yoochoose_schema_file):
-    schema = DatasetSchema.from_schema(str(yoochoose_schema_file))
+    schema = DatasetSchema.from_proto(str(yoochoose_schema_file))
 
     assert len(schema.columns) == 20
     assert len(schema.select_by_tag(Tag.CONTINUOUS).columns) == 6
@@ -21,7 +21,7 @@ def test_schema_from_yoochoose_schema(yoochoose_schema_file):
 
 def test_schema_embedding_sizes_nvt(yoochoose_schema_file):
     pytest.importorskip("nvtabular")
-    schema = DatasetSchema.from_schema(str(yoochoose_schema_file))
+    schema = DatasetSchema.from_proto(str(yoochoose_schema_file))
 
     assert schema.cardinalities() == {"item_id/list": 51996, "category/list": 332}
     embedding_sizes = schema.embedding_sizes_nvt(minimum_size=16, maximum_size=512)
@@ -29,7 +29,7 @@ def test_schema_embedding_sizes_nvt(yoochoose_schema_file):
 
 
 def test_schema_embedding_sizes(yoochoose_schema_file):
-    schema = DatasetSchema.from_schema(str(yoochoose_schema_file))
+    schema = DatasetSchema.from_proto(str(yoochoose_schema_file))
 
     assert schema.cardinalities() == {"item_id/list": 51996, "category/list": 332}
     embedding_sizes = schema.embedding_sizes(multiplier=3.0)
