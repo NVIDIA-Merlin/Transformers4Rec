b6219a69b075f45bbd542564329f94cb510859f7
Marc Romeyn <marcromeyn@gmail.com>
2021-09-16 09:50:00 +0200

    Adding test_generate_item_interactions

--- tests/data/test_synthetic.py
+++ tests/data/test_synthetic.py
@@ -1,41 +1,32 @@
-import merlin_standard_lib as msl
-from merlin_standard_lib import Tag
-from transformers4rec.data.synthetic import generate_session_interactions
+import pytest
+
+from transformers4rec.data.synthetic import generate_item_interactions
+
+pd = pytest.importorskip("pandas")
 
 
-def test_synthetic_data():
-    schema = msl.Schema(
-        [
-            msl.ColumnSchema.create_categorical("session_id", num_items=5000, tags=["session_id"]),
-            msl.ColumnSchema.create_categorical(
-                "item_id",
-                num_items=10000,
-                tags=[Tag.ITEM_ID, Tag.LIST],
-                value_count=msl.schema.ValueCount(1, 20),
-            ),
-            msl.ColumnSchema.create_categorical(
-                "category",
-                num_items=100,
-                tags=[Tag.LIST, Tag.ITEM],
-                value_count=msl.schema.ValueCount(1, 20),
-            ),
-            msl.ColumnSchema.create_continuous(
-                "item_recency",
-                min_value=0,
-                max_value=1,
-                tags=[Tag.LIST, Tag.ITEM],
-                value_count=msl.schema.ValueCount(1, 20),
-            ),
-            msl.ColumnSchema.create_categorical("day", num_items=11, tags=[Tag.SESSION]),
-            msl.ColumnSchema.create_categorical(
-                "purchase", num_items=3, tags=[Tag.SESSION, Tag.BINARY_CLASSIFICATION]
-            ),
-            msl.ColumnSchema.create_continuous(
-                "price", min_value=0, max_value=1, tags=[Tag.SESSION, Tag.REGRESSION]
-            ),
-        ]
-    )
+def test_generate_item_interactions(synthetic_schema):
+    data = generate_item_interactions(500, synthetic_schema)
 
-    data = generate_session_interactions(100, schema, 30, 5, "cpu")
+    assert isinstance(data, pd.DataFrame)
+    assert len(data) == 500
+    assert list(data.columns) == [
+        "session_id",
+        "item_id",
+        "day",
+        "purchase",
+        "price",
+        "category",
+        "item_recency",
+    ]
+    expected_dtypes = {
+        "session_id": "int64",
+        "item_id": "int64",
+        "day": "int64",
+        "purchase": "int64",
+        "price": "float64",
+        "category": "int64",
+        "item_recency": "float64",
+    }
 
-    assert data is not None
+    assert all(val == expected_dtypes[key] for key, val in dict(data.dtypes).items())
