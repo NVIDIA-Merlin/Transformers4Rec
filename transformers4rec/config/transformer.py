5f37f87ba41cd64129ac8a4c8803984683bd8597
root <ronayak@hotmail.com>
2021-12-02 20:19:28 +0000

    modify end-to-end nbs and add tf notebook

--- transformers4rec/config/transformer.py
+++ transformers4rec/config/transformer.py
@@ -15,7 +15,7 @@
 #
 
 import transformers
-
+import tensorflow as tf
 from merlin_standard_lib import Registry
 
 transformer_registry: Registry = Registry("transformers")
@@ -64,7 +64,7 @@ class T4RecConfig:
         *prediction_task,
         task_blocks=None,
         task_weights=None,
-        loss_reduction="mean",
+        loss_reduction=tf.reduce_mean,
         **kwargs
     ):
         from .. import tf as tf4rec
