9b80f4cde9ec3370d44066ee36a5824f51bd872c
root <ronayak@hotmail.com>
2022-12-28 15:15:39 +0000

    remove serving part from unit test and skip old unit test

--- tests/unit/test_getting_started_session_based.py
+++ tests/unit/test_getting_started_session_based.py
@@ -3,7 +3,6 @@ from importlib.util import find_spec
 
 import numpy as np
 import pytest
-from merlin.systems.triton.utils import run_ensemble_on_tritonserver
 from testbook import testbook
 
 from tests.conftest import REPO_ROOT
@@ -64,51 +63,3 @@ def test_func():
             ]
         )
         assert os.path.isdir("/tmp/data/saved_model")
-
-    with testbook(
-        REPO_ROOT
-        / "examples"
-        / "getting-started-session-based"
-        / "03-serving-session-based-model-torch-backend.ipynb",
-        execute=False,
-    ) as tb3:
-        tb3.inject(
-            """
-            import os
-            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
-            os.environ["INPUT_SCHEMA_PATH"] = "/tmp/data/processed_nvt/schema.pbtxt"
-            os.environ["model_path"] = "/tmp/data/saved_model"
-            """
-        )
-        NUM_OF_CELLS = len(tb3.cells)
-        tb3.execute_cell(list(range(0, NUM_OF_CELLS - 10)))
-        tb3.inject(
-            """
-            eval_batch_size = 4
-            eval_paths = os.path.join('/tmp/data/sessions_by_day', f"{1}/valid.parquet")
-            eval_dataset = Dataset(eval_paths, shuffle=False)
-            eval_loader = generate_dataloader(schema, eval_dataset, batch_size=eval_batch_size)
-            test_dict = next(iter(eval_loader))
-
-            df_cols = {}
-            for name, tensor in test_dict[0].items():
-                if name in input_schema.column_names:
-                    dtype = input_schema[name].dtype
-                    df_cols[name] = tensor.cpu().numpy().astype(dtype)
-                    if len(tensor.shape) > 1:
-                        df_cols[name] = list(df_cols[name])
-
-            df = make_df(df_cols)
-            
-            
-            from merlin.systems.triton.utils import run_ensemble_on_tritonserver
-            response = run_ensemble_on_tritonserver(
-                "/tmp/data/models", ensemble.graph.input_schema, df[input_schema.column_names], output_schema.column_names,  'executor_model'
-            )
-            response_array = [x.tolist()[0] for x in response["next-item"]]
-            """
-        )
-        tb3.execute_cell(NUM_OF_CELLS - 3)
-        batch_size = tb3.ref("eval_batch_size")
-        response_array = tb3.ref("response_array")
-        assert len(response_array) == batch_size
