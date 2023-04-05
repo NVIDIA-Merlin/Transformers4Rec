08c79d69b2fa9e8064d3eb70bbb3d2d8bddd8ffd
Oliver Holworthy <oholworthy@nvidia.com>
2023-04-05 18:17:37 +0100

    Add tests for pad_inputs

--- tests/unit/utils/test_padding.py
+++ tests/unit/utils/test_padding.py
@@ -17,7 +17,7 @@ from itertools import accumulate
 
 import torch
 
-from transformers4rec.torch.utils.padding import pad_batch
+from transformers4rec.torch.utils.padding import pad_batch, pad_inputs
 
 
 def _get_values_offsets(data):
@@ -30,81 +30,127 @@ def _get_values_offsets(data):
     return torch.tensor(values), torch.tensor(offsets)
 
 
-def test_pad_values_offsets_dict():
-    data = [[1, 2], [], [3, 4, 5]]
-    values, offsets = _get_values_offsets(data)
+class TestPadBatch:
+    def test_pad_values_offsets(self):
+        data = [[1, 2], [], [3, 4, 5]]
+        values, offsets = _get_values_offsets(data)
 
-    x = {"a__values": values, "a__offsets": offsets}
+        x = {"a__values": values, "a__offsets": offsets}
 
-    padded_x = pad_batch(x, {"a": 7})
-    assert torch.equal(
-        padded_x["a"],
-        torch.tensor(
-            [
-                [1, 2, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [3, 4, 5, 0, 0, 0, 0],
-            ]
-        ),
-    )
+        padded_x = pad_batch(x, {"a": 7})
+        assert torch.equal(
+            padded_x["a"],
+            torch.tensor(
+                [
+                    [1, 2, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [3, 4, 5, 0, 0, 0, 0],
+                ]
+            ),
+        )
+
+    def test_with_truncation(self):
+        data = [[1, 2], [], [3, 4, 5, 4, 7]]
+        values, offsets = _get_values_offsets(data)
+
+        x = {
+            "a__values": values,
+            "a__offsets": offsets,
+            "b": torch.tensor([[1, 2, 3, 4], [6, 7, 8, 9]]),
+        }
+
+        padded_x = pad_batch(x, {"a": 3, "b": 2})
+        assert torch.equal(
+            padded_x["a"],
+            torch.tensor(
+                [
+                    [1, 2, 0],
+                    [0, 0, 0],
+                    [3, 4, 5],
+                ]
+            ),
+        )
+        assert torch.equal(
+            padded_x["b"],
+            torch.tensor(
+                [
+                    [1, 2],
+                    [6, 7],
+                ]
+            ),
+        )
+
+    def test_ragged_and_dense(self):
+        data = [[1, 2], [], [3, 4, 5]]
+        values, offsets = _get_values_offsets(data)
+
+        x = {
+            "a__values": values,
+            "a__offsets": offsets,
+            "b": torch.tensor([[3, 6], [4, 1], [8, 4]]),
+        }
+
+        padded_x = pad_batch(x, {"a": 7, "b": 3})
+        assert torch.equal(
+            padded_x["a"],
+            torch.tensor(
+                [
+                    [1, 2, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [3, 4, 5, 0, 0, 0, 0],
+                ]
+            ),
+        )
+        assert torch.equal(
+            padded_x["b"],
+            torch.tensor(
+                [
+                    [3, 6, 0],
+                    [4, 1, 0],
+                    [8, 4, 0],
+                ]
+            ),
+        )
 
 
-def test_pad_with_truncation():
-    data = [[1, 2], [], [3, 4, 5, 4, 7]]
-    values, offsets = _get_values_offsets(data)
+class TestPadInputs:
+    def test_ragged_inputs(self):
+        data = [[1, 2, 3, 4, 5], [6, 7, 8]]
+        values, offsets = _get_values_offsets(data)
 
-    x = {
-        "a__values": values,
-        "a__offsets": offsets,
-        "b": torch.tensor([[1, 2, 3, 4], [6, 7, 8, 9]]),
-    }
+        inputs = {"a__values": values, "a__offsets": offsets}
+        padded_inputs = pad_inputs(inputs)
+        assert torch.equal(
+            padded_inputs["a"],
+            torch.tensor(
+                [
+                    [1, 2, 3, 4, 5],
+                    [6, 7, 8, 0, 0],
+                ]
+            ),
+        )
 
-    padded_x = pad_batch(x, {"a": 3, "b": 2})
-    assert torch.equal(
-        padded_x["a"],
-        torch.tensor(
-            [
-                [1, 2, 0],
-                [0, 0, 0],
-                [3, 4, 5],
-            ]
-        ),
-    )
-    assert torch.equal(
-        padded_x["b"],
-        torch.tensor(
-            [
-                [1, 2],
-                [6, 7],
-            ]
-        ),
-    )
+    def test_with_max_sequence_length(self):
+        data = [[1, 2, 3, 4, 5], [6, 7, 8, 9]]
+        values, offsets = _get_values_offsets(data)
 
-
-def test_pad_values_dense():
-    data = [[1, 2], [], [3, 4, 5]]
-    values, offsets = _get_values_offsets(data)
-
-    x = {"a__values": values, "a__offsets": offsets, "b": torch.tensor([[3, 6], [4, 1], [8, 4]])}
-
-    padded_x = pad_batch(x, {"a": 7, "b": 3})
-    assert torch.equal(
-        padded_x["a"],
-        torch.tensor(
-            [
-                [1, 2, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [3, 4, 5, 0, 0, 0, 0],
-            ]
-        ),
-    )
-    assert torch.equal(
-        padded_x["b"],
-        torch.tensor(
-            [
-                [3, 6, 0],
-                [4, 1, 0],
-                [8, 4, 0],
-            ]
-        ),
-    )
+        inputs = {"a__values": values, "a__offsets": offsets, "b": torch.tensor([[3, 6], [4, 1]])}
+        padded_inputs = pad_inputs(inputs, max_sequence_length=3)
+        assert torch.equal(
+            padded_inputs["a"],
+            torch.tensor(
+                [
+                    [1, 2, 3],
+                    [6, 7, 8],
+                ]
+            ),
+        )
+        assert torch.equal(
+            padded_inputs["b"],
+            torch.tensor(
+                [
+                    [3, 6],
+                    [4, 1],
+                ]
+            ),
+        )
