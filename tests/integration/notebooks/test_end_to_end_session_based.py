d24915a1e45684ff6b01b44f57421f8437aa7621
root <bbozkaya@gmail.com>
2023-04-12 19:50:48 +0000

    added test for notebook 03

--- tests/integration/notebooks/test_end_to_end_session_based.py
+++ tests/integration/notebooks/test_end_to_end_session_based.py
@@ -25,6 +25,7 @@ def test_func():
             os.environ["USE_SYNTHETIC"] = "True"
             os.environ["START_DATE"] = "2014/4/1"
             os.environ["END_DATE"] = "2014/4/5"
+            os.environ["THRESHOLD_DAY_INDEX"] = "1"
             """
         )
         tb1.execute()
@@ -32,45 +33,24 @@ def test_func():
         assert os.path.isdir("/tmp/data/preproc_sessions_by_day")
         assert os.path.isdir("/tmp/data/workflow_etl")
 
-    # with testbook(
-    #     REPO_ROOT
-    #     / "examples"
-    #     / "end-to-end-session-based"
-    #     / "03-Session-based-Yoochoose-multigpu-training-PyT.ipynb",
-    #     execute=False,
-    # ) as tb3:
-    #     tb3.inject(
-    #         """
-    #         import os
-    #         os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
-    #         os.environ["OUTPUT_DIR"] = "/tmp/data/sessions_by_day"
-    #         os.environ["model_path"] = "/tmp/data/saved_model"
-    #         """
-    #     )
-    #     NUM_OF_CELLS = len(tb3.cells)
-    #     tb3.execute_cell(list(range(0, NUM_OF_CELLS - 12)))
-    #     tb3.inject(
-    #         """
-    #         eval_batch_size = 4
-    #         eval_paths = os.path.join('/tmp/data/sessions_by_day', f"{1}/valid.parquet")
-    #         eval_dataset = Dataset(eval_paths, shuffle=False)
-    #         eval_loader = generate_dataloader(schema, eval_dataset, batch_size=eval_batch_size)
-    #         test_dict = next(iter(eval_loader))
-    #         df_cols = {}
-    #         for name, tensor in test_dict[0].items():
-    #             if name in input_schema.column_names:
-    #                 df_cols[name] = tensor.cpu().numpy()
-    #                 if len(tensor.shape) > 1:
-    #                     df_cols[name] = list(df_cols[name])
-    #         df = make_df(df_cols)
-    #         from merlin.systems.triton.utils import run_ensemble_on_tritonserver
-    #         response = run_ensemble_on_tritonserver(
-    #             "/tmp/data/models", ensemble.graph.input_schema, df[input_schema.column_names], output_schema.column_names,  'executor_model'
-    #         )
-    #         response_array = [x.tolist()[0] for x in response["next-item"]]
-    #         """
-    #     )
-    #     tb3.execute_cell(NUM_OF_CELLS - 3)
-    #     batch_size = tb3.ref("eval_batch_size")
-    #     response_array = tb3.ref("response_array")
-    #     assert len(response_array) == batch_size
+    with testbook(
+        REPO_ROOT
+        / "examples"
+        / "end-to-end-session-based"
+        / "03-Session-based-Yoochoose-multigpu-training-PyT.ipynb",
+        execute=False,
+    ) as tb3:
+        tb3.inject(
+            """
+            import os
+            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
+            os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
+            os.environ["USE_SYNTHETIC"] = "True"
+            os.environ["START_DAY_INDEX"] = "1"
+            os.environ["FINAL_DAY_INDEX"] = "4"
+            os.environ["LEARNING_RATE"] = "0.0005"
+            os.environ["BATCH_SIZE_TRAIN"] = "64"
+            os.environ["BATCH_SIZE_VALID"] = "32"
+            """
+        )
+        tb3.execute()
