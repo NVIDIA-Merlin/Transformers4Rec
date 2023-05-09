06aabc75574fc13ac84f741b8a9e15f029de4fe2
Burcin Bozkaya <bbozkaya@gmail.com>
2023-05-09 18:15:06 -0400

    update

--- tests/integration/notebooks/test_end_to_end_session_based.py
+++ tests/integration/notebooks/test_end_to_end_session_based.py
@@ -37,6 +37,48 @@ def test_func():
         REPO_ROOT
         / "examples"
         / "end-to-end-session-based"
+        / "02-End-to-end-session-based-with-Yoochoose-PyT.ipynb",
+        timeout=720,
+        execute=False,
+    ) as tb2:
+        tb2.inject(
+            """
+            import os
+            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
+            os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
+            os.environ["START_TIME_INDEX"] = "1"
+            os.environ["END_TIME_INDEX"] = "3"
+            os.environ["BATCH_SIZE_TRAIN"] = "64"
+            os.environ["BATCH_SIZE_VALID"] = "32"
+            """
+        )
+        NUM_OF_CELLS = len(tb2.cells)
+        tb2.execute_cell(list(range(0, NUM_OF_CELLS - 20)))
+        topk = tb2.ref("topk")
+        tb2.inject(
+            """
+            import pandas as pd
+            interactions_merged_df = pd.read_parquet(os.path.join(INPUT_DATA_DIR, "interactions_merged_df.parquet"))
+            interactions_merged_df = interactions_merged_df.sort_values('timestamp')
+            batch = interactions_merged_df[-500:]
+            sessions_to_use = batch.session_id.value_counts()
+            filtered_batch = batch[batch.session_id.isin(sessions_to_use[sessions_to_use.values>1].index.values)]
+            
+            from merlin.systems.triton.utils import run_ensemble_on_tritonserver
+            response = run_ensemble_on_tritonserver(
+                "/tmp/data/models", workflow.input_schema, filtered_batch, model.output_schema.column_names,  'executor_model'
+            )
+            response_array = list(response['item_ids'][1])
+            """
+        )
+        tb2.execute_cell(NUM_OF_CELLS - 8)
+        response_array = tb2.ref("response_array")
+        assert len(response_array) == topk
+
+    with testbook(
+        REPO_ROOT
+        / "examples"
+        / "end-to-end-session-based"
         / "03-Session-based-Yoochoose-multigpu-training-PyT.ipynb",
         execute=False,
     ) as tb3:
@@ -45,9 +87,8 @@ def test_func():
             import os
             os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
             os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
-            os.environ["USE_SYNTHETIC"] = "True"
-            os.environ["START_DAY_INDEX"] = "1"
-            os.environ["FINAL_DAY_INDEX"] = "4"
+            os.environ["START_TIME_INDEX"] = "1"
+            os.environ["END_TIME_INDEX"] = "4"
             os.environ["LEARNING_RATE"] = "0.0005"
             os.environ["BATCH_SIZE_TRAIN"] = "64"
             os.environ["BATCH_SIZE_VALID"] = "32"
