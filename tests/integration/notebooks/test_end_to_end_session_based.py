bd81dcddf16894126b4b3862f45d90b73ce1ca68
root <bbozkaya@gmail.com>
2023-05-22 17:34:08 +0000

    removed nb3 test due to multi-gpu freezing issue

--- tests/integration/notebooks/test_end_to_end_session_based.py
+++ tests/integration/notebooks/test_end_to_end_session_based.py
@@ -56,26 +56,3 @@ def test_func():
         tb2.execute_cell(list(range(0, NUM_OF_CELLS - 20)))
         assert os.path.isdir("/tmp/data/models")
         assert os.listdir("/tmp/data/models")
-
-    with testbook(
-        REPO_ROOT
-        / "examples"
-        / "end-to-end-session-based"
-        / "03-Session-based-Yoochoose-multigpu-training-PyT.ipynb",
-        timeout=720,
-        execute=False,
-    ) as tb3:
-        tb3.inject(
-            """
-            import os
-            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
-            os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
-            os.environ["START_TIME_INDEX"] = "1"
-            os.environ["END_TIME_INDEX"] = "4"
-            os.environ["LEARNING_RATE"] = "0.0005"
-            os.environ["BATCH_SIZE_TRAIN"] = "64"
-            os.environ["BATCH_SIZE_VALID"] = "32"
-            """
-        )
-        tb3.execute()
-        assert os.path.isfile("/tmp/data/eval_metrics.txt")
