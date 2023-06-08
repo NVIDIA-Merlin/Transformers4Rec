794365cce357edea508bf7dc78a125dd7414979c
Sara Rabhi <sara.rabhi@gmail.com>
2023-06-08 09:58:09 -0400

    Add missing docstrings (#706)
    * add missing docstrings
    
    * ignore type to fix mypy issues
    
    ---------
    
    Co-authored-by: rnyak <16246900+rnyak@users.noreply.github.com>
    Co-authored-by: Oliver Holworthy <oholworthy@nvidia.com>

--- transformers4rec/torch/block/mlp.py
+++ transformers4rec/torch/block/mlp.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import torch
 
@@ -21,13 +21,36 @@ from .base import BuildableBlock, SequentialBlock
 
 
 class MLPBlock(BuildableBlock):
+    """Defines Multi-Layer Perceptron (MLP) Block by stacking
+    multiple DenseBlock instances.
+
+    Parameters
+    ----------
+    dimensions : int or list of int
+        The dimensions of the layers in the MLP.
+        If an integer is provided, a single layer MLP is created.
+        If a list is provided, it must contain the size of each layer in order.
+    activation : optional
+        The activation function to apply after each layer.
+        By default `torch.nn.ReLU`.
+    use_bias : bool, optional
+        Whether to add a bias term to the dense layers.
+        by default True
+    dropout : float, optional
+        The dropout rate to apply after each layer, by default None
+    normalization : str, optional
+        The normalization to apply after each layer, by default None
+    filter_features : List[str], optional
+        List of features to select from the input., by default None
+    """
+
     def __init__(
         self,
         dimensions,
         activation=torch.nn.ReLU,
         use_bias: bool = True,
-        dropout=None,
-        normalization=None,
+        dropout: float = None,
+        normalization: str = None,
         filter_features=None,
     ) -> None:
         super().__init__()
@@ -65,9 +88,41 @@ class MLPBlock(BuildableBlock):
 
 
 class DenseBlock(SequentialBlock):
+    """
+    A buildable dense Block to represent a fully connected layer.
+
+    Parameters
+    ----------
+    input_shape : Union[List[int], torch.Size]
+        The shape of the input tensor.
+
+    in_features : int
+        Number of input features.
+
+    out_features : int
+        Number of output features.
+
+    activation : torch.nn.Module, optional
+        The activation function to apply after the linear layer.
+        By default `torch.nn.ReLU`.
+
+    use_bias : bool, optional
+        Whether to use bias in the layer.
+        By default True.
+
+    dropout : float, optional
+        The dropout rate to apply after the dense layer, if any.
+        By default is None.
+
+    normalization : str, optional
+        The type of normalization to apply after the dense layer.
+        Only 'batch_norm' is supported.
+        By default is None.
+    """
+
     def __init__(
         self,
-        input_shape,
+        input_shape: Union[List[int], torch.Size],
         in_features: int,
         out_features: int,
         activation=torch.nn.ReLU,
