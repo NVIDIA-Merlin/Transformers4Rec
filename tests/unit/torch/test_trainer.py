fa839255cc15e406a9bcb6e953c7146b5b726cb7
Gabriel Moreira <gmoreira@nvidia.com>
2023-06-13 20:58:38 -0300

    Fixed OOM issues when evaluating/predicting

--- tests/unit/torch/test_trainer.py
+++ tests/unit/torch/test_trainer.py
@@ -573,17 +573,79 @@ def test_trainer_with_multiple_tasks(schema_type):
     assert predictions.predictions["click/binary_classification_task"].shape == (1000,)
 
 
-def test_trainer_trop_k_with_wrong_task():
+@pytest.mark.parametrize("predict_top_k", [20, None, "default"])
+def test_trainer_predict_topk(predict_top_k):
+    DEFAULT_PREDICT_TOP_K = 100
+
     data = tr.data.music_streaming_testing_data
     schema = data.schema
     batch_size = 16
-    predict_top_k = 20
 
-    task = tr.BinaryClassificationTask("click", summary_type="mean")
+    task = tr.NextItemPredictionTask(weight_tying=True)
     inputs = tr.TabularSequenceFeatures.from_schema(
         schema,
         max_sequence_length=20,
         d_output=64,
+        masking="clm",
+    )
+    transformer_config = tconf.XLNetConfig.build(64, 4, 2, 20)
+    model = transformer_config.to_torch_model(inputs, task)
+
+    additional_args = {}
+    if not isinstance(predict_top_k, str):
+        additional_args["predict_top_k"] = predict_top_k
+
+    args = trainer.T4RecTrainingArguments(
+        output_dir=".",
+        num_train_epochs=1,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size // 2,
+        data_loader_engine="merlin_dataloader",
+        max_sequence_length=20,
+        report_to=[],
+        debug=["r"],
+        **additional_args,
+    )
+
+    recsys_trainer = tr.Trainer(
+        model=model,
+        args=args,
+        schema=schema,
+        train_dataset_or_path=data.path,
+        eval_dataset_or_path=data.path,
+        test_dataset_or_path=data.path,
+        compute_metrics=True,
+    )
+
+    outputs = recsys_trainer.predict(data.path)
+
+    if predict_top_k is None:
+        assert outputs.predictions.shape[1] == 10001
+    else:
+        if predict_top_k == "default":
+            predict_top_k = DEFAULT_PREDICT_TOP_K
+
+        pred_item_ids, pred_scores = outputs.predictions
+        assert len(pred_item_ids.shape) == 2
+        assert pred_item_ids.shape[1] == predict_top_k
+        assert len(pred_scores.shape) == 2
+        assert pred_scores.shape[1] == predict_top_k
+
+
+@pytest.mark.parametrize("predict_top_k", [15, 20, 30, None])
+@pytest.mark.parametrize("top_k", [20, None])
+def test_trainer_predict_top_k_x_top_k(predict_top_k, top_k):
+    data = tr.data.music_streaming_testing_data
+    schema = data.schema
+    batch_size = 16
+
+    task = tr.NextItemPredictionTask(weight_tying=True)
+
+    inputs = tr.TabularSequenceFeatures.from_schema(
+        schema,
+        max_sequence_length=20,
+        d_output=64,
+        masking="clm",
     )
     transformer_config = tconf.XLNetConfig.build(64, 4, 2, 20)
     model = transformer_config.to_torch_model(inputs, task)
@@ -609,10 +671,28 @@ def test_trainer_trop_k_with_wrong_task():
         test_dataset_or_path=data.path,
         compute_metrics=True,
     )
-    with pytest.raises(AssertionError) as excinfo:
-        recsys_trainer.predict(data.path)
 
-    assert "Top-k prediction is specific to NextItemPredictionTask" in str(excinfo.value)
+    model.top_k = top_k
+
+    if predict_top_k and top_k and predict_top_k > top_k:
+        with pytest.raises(ValueError) as excinfo:
+            recsys_trainer.predict(data.path)
+        assert "The args.predict_top_k should not be larger than model.top_k" in str(excinfo.value)
+
+    else:
+        outputs = recsys_trainer.predict(data.path)
+
+        if predict_top_k or top_k:
+            expected_top_k = predict_top_k or top_k
+
+            pred_item_ids, pred_scores = outputs.predictions
+            assert len(pred_item_ids.shape) == 2
+            assert pred_item_ids.shape[1] == expected_top_k
+            assert len(pred_scores.shape) == 2
+            assert pred_scores.shape[1] == expected_top_k
+        else:
+            ITEM_CARDINALITY = 10001
+            assert outputs.predictions.shape[1] == ITEM_CARDINALITY
 
 
 def test_trainer_with_pretrained_embeddings():
