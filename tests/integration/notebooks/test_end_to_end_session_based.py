405ed68653e4570f6594f41132b2d2bdad4815ec
root <bbozkaya@gmail.com>
2023-07-05 22:08:02 +0000

    update test py

--- tests/integration/notebooks/test_end_to_end_session_based.py
+++ tests/integration/notebooks/test_end_to_end_session_based.py
@@ -10,29 +10,29 @@ pytest.importorskip("transformers")
 
 # flake8: noqa
 
-
 @pytest.mark.notebook
 @pytest.mark.skipif(not HAS_GPU, reason="No GPU available")
-def test_func():
+def test_func(tmp_path):
     with testbook(
         REPO_ROOT / "examples" / "end-to-end-session-based" / "01-ETL-with-NVTabular.ipynb",
         execute=False,
     ) as tb1:
+        dirname = f"{tmp_path}/data"
+        os.mkdir(dirname)
         tb1.inject(
-            """
+            f"""
             import os
-            os.environ["DATA_FOLDER"] = "/tmp/data/"
+            os.environ["DATA_FOLDER"] = f"{dirname}"
             os.environ["USE_SYNTHETIC"] = "True"
             os.environ["START_DATE"] = "2014/4/1"
             os.environ["END_DATE"] = "2014/4/5"
             os.environ["THRESHOLD_DAY_INDEX"] = "1"
             """
         )
-        print("\nexecuting tb1...")
         tb1.execute()
-        assert os.path.isdir("/tmp/data/processed_nvt")
-        assert os.path.isdir("/tmp/data/preproc_sessions_by_day")
-        assert os.path.isdir("/tmp/data/workflow_etl")
+        assert os.path.isdir(f"{dirname}/processed_nvt")
+        assert os.path.isdir(f"{dirname}/preproc_sessions_by_day")
+        assert os.path.isdir(f"{dirname}/workflow_etl")
 
     with testbook(
         REPO_ROOT
@@ -42,11 +42,12 @@ def test_func():
         timeout=720,
         execute=False,
     ) as tb2:
+        dirname = f"{tmp_path}/data"
         tb2.inject(
-            """
+            f"""
             import os
-            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
-            os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
+            os.environ["INPUT_DATA_DIR"] = f"{dirname}"
+            os.environ["OUTPUT_DIR"] = f"{dirname}/preproc_sessions_by_day"
             os.environ["START_TIME_INDEX"] = "1"
             os.environ["END_TIME_INDEX"] = "3"
             os.environ["BATCH_SIZE_TRAIN"] = "64"
@@ -54,10 +55,9 @@ def test_func():
             """
         )
         NUM_OF_CELLS = len(tb2.cells)
-        print("executing tb2...")
         tb2.execute_cell(list(range(0, NUM_OF_CELLS - 20)))
-        assert os.path.isdir("/tmp/data/models")
-        assert os.listdir("/tmp/data/models")
+        assert os.path.isdir(f"{dirname}/models")
+        assert os.listdir(f"{dirname}/models")
 
     with testbook(
         REPO_ROOT
@@ -67,11 +67,12 @@ def test_func():
         timeout=720,
         execute=False,
     ) as tb3:
+        dirname = f"{tmp_path}/data"
         tb3.inject(
-            """
+            f"""
             import os
-            os.environ["INPUT_DATA_DIR"] = "/tmp/data/"
-            os.environ["OUTPUT_DIR"] = "/tmp/data/preproc_sessions_by_day"
+            os.environ["INPUT_DATA_DIR"] = f"{dirname}"
+            os.environ["OUTPUT_DIR"] = f"{dirname}/preproc_sessions_by_day"
             os.environ["START_TIME_INDEX"] = "1"
             os.environ["END_TIME_INDEX"] = "4"
             os.environ["LEARNING_RATE"] = "0.0005"
@@ -79,6 +80,5 @@ def test_func():
             os.environ["BATCH_SIZE_VALID"] = "32"
             """
         )
-        print("executing tb3...")
         tb3.execute()
-        assert os.path.isfile("/tmp/data/eval_metrics.txt")
+        assert os.path.isfile(f"{dirname}/eval_metrics.txt")
